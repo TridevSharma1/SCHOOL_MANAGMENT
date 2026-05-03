9cfa770c1ad6b671b79bea14e2b6bc2433069676
TridevSharma1 <tridevx8@gmail.com>
2026-05-03 17:33:43 +0530

    .PY

--- Schoolmanagment/managmentsystem/models.py
+++ Schoolmanagment/managmentsystem/models.py
@@ -1,3 +1,15 @@
-from django.db import models
 
 # Create your models here.
+from django.db import models
+
+class Student(models.Model):
+    name = models.CharField(max_length=100)
+    student_class = models.CharField(max_length=50)
+    city = models.CharField(max_length=100)
+    age = models.PositiveIntegerField()
+    address = models.TextField()
+    image = models.ImageField(upload_to='students/')
+    roll_no = models.CharField(max_length=20, unique=True)
+
+    def __str__(self):
+        return f"{self.name} ({self.roll_no})"
--- Schoolmanagment/managmentsystem/views.py
+++ Schoolmanagment/managmentsystem/views.py
@@ -1,3 +1,52 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Student
 
-# Create your views here.
+# 📌 READ (List)
+def student_list(request):
+    students = Student.objects.all()
+    return render(request, 'student_list.html', {'students': students})
+
+
+# 📌 CREATE
+def add_student(request):
+    if request.method == "POST":
+        Student.objects.create(
+            name=request.POST.get('name'),
+            student_class=request.POST.get('student_class'),
+            city=request.POST.get('city'),
+            age=request.POST.get('age'),
+            address=request.POST.get('address'),
+            roll_no=request.POST.get('roll_no'),
+            image=request.FILES.get('image')
+        )
+        return redirect('student_list')
+
+    return render(request, 'add_student.html')
+
+
+# 📌 UPDATE
+def update_student(request, id):
+    student = get_object_or_404(Student, id=id)
+
+    if request.method == "POST":
+        student.name = request.POST.get('name')
+        student.student_class = request.POST.get('student_class')
+        student.city = request.POST.get('city')
+        student.age = request.POST.get('age')
+        student.address = request.POST.get('address')
+        student.roll_no = request.POST.get('roll_no')
+
+        if request.FILES.get('image'):
+            student.image = request.FILES.get('image')
+
+        student.save()
+        return redirect('student_list')
+
+    return render(request, 'update_student.html', {'student': student})
+
+
+# 📌 DELETE
+def delete_student(request, id):
+    student = get_object_or_404(Student, id=id)
+    student.delete()
+    return redirect('student_list')
